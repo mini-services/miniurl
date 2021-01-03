f12f043f8bd5092fc34f43b469c338774441a807
yakir fitousi <yakafi2c@gmail.com>
2021-01-04 01:29:29 +0200

    [WIP] set up tsconfig for tests, write first test for happy flow on validate configuration

--- src/config/__test__/validate.test.js
+++
@@ -1,7 +0,0 @@
-import test from 'ava';
-
-const fn = () => 'foo';
-
-test('mock test for first running', t => {
-	t.is(fn(), 'foo');
-});
---
+++ src/config/__test__/validate.test.ts
@@ -0,0 +1,29 @@
+import { validateConfig } from '../validate'
+import test from 'ava'
+import { RawConfig } from '../types.js'
+
+const config: RawConfig = {
+	port: 'mock-port',
+	appName: 'mini-test',
+	baseRedirectUrl: 'http://mock.com',
+	url: {
+		matchPattern: 'mock-patter',
+		lifetime: '120',
+	},
+	storage: {
+		driverName: 'InMemory',
+		relationalDriverConfig: {
+			client: 'mock-client',
+			connection: {
+				host: 'local-host',
+				user: 'snir-hamair',
+				password: 'admin1234',
+				database: 'mock-db',
+			},
+		},
+	},
+}
+
+test('validate config - happy flow [configuration set propely]', (t) => {
+	t.is(validateConfig(config), true)
+})
