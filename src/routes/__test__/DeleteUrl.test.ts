40f0804e25aeb39ab427bde91128195f180ee8c7
Daniel Giat <danielg@outbrain.com>
2021-05-19 14:39:02 +0300

    test(crud): PUT + DELETE test + Postman urls full collection

---
+++ src/routes/__test__/DeleteUrl.test.ts
@@ -0,0 +1,18 @@
+import { mockCreateApp } from '../../__test__/mockApp'
+import { routes } from '../index'
+import { mockId } from '../../services/storage/__test__/mock'
+
+test('DELETE /url/:id', async () => {
+	const app = await mockCreateApp()
+	app.register(routes)
+
+  app.storage = { url: { delete: jest.fn().mockResolvedValue(true) } } as any
+
+  const response = await app.inject({
+		method: 'DELETE',
+		url: `${app.config.apiPrefix}/url/${mockId}`
+	})
+
+	expect(response.statusCode).toBe(200)
+	expect(response.body).toBeTruthy()
+})
---
+++ src/routes/__test__/modifyUrl.test.ts
@@ -0,0 +1,29 @@
+import { mockCreateApp } from '../../__test__/mockApp'
+import { routes } from '../index'
+import { mockId } from '../../services/storage/__test__/mock'
+
+test('PUT /url/:id', async () => {
+	const app = await mockCreateApp()
+	app.register(routes)
+
+	const urlMock = 'https://twitter.com/elonmusk'
+
+	const storedUrl = {
+		id: '123',
+		url: urlMock,
+		createdAt: new Date().toISOString(),
+		updatedAt: new Date().toISOString(),
+	}
+	app.storage = { url: { edit: jest.fn().mockResolvedValue(storedUrl) } } as any
+
+	const response = await app.inject({
+		method: 'PUT',
+		url: `${app.config.apiPrefix}/url/${mockId}`,
+		payload: {
+			url: urlMock,
+		},
+	})
+
+	expect(response.statusCode).toBe(200)
+	expect(JSON.parse(response.body).url).toEqual(urlMock)
+})
