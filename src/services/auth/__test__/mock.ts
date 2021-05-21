a7e20a8342fb4dcdd0e25827661d4eabec943414
Daniel G <blaaarp@gmail.com>
2021-05-21 15:42:20 +0300

    [v0.1.72] routes unit tests (#194)
    * feat: crud - PUT + DELETE operations
    
    * feat: crud - PUT + DELETE operations
    
    * test(crud): PUT + DELETE test + Postman urls full collection
    
    * test(crud): PUT + DELETE test + Postman urls full collection
    
    Co-authored-by: Daniel Giat <danielg@outbrain.com>

--- src/services/auth/__test__/mock.ts
+++ src/services/auth/__test__/mock.ts
@@ -2,5 +2,6 @@ const mockIsAuthorized = jest.fn().mockResolvedValue(true)
 export const MockAuth = jest.fn().mockImplementation(() => {
 	return {
 		isAuthorized: mockIsAuthorized,
+		authorize: mockIsAuthorized
 	}
 })
